91fde49950b47aeb977dcf9060ecc8419ba6b696
Rehman ch <124032207+Rehmanch788@users.noreply.github.com>
2026-04-22 09:47:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(express.static('public'));
+
 module.exports.app = app;
-module.exports.port = port
+module.exports.port = port;
